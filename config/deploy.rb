fc04bb74987841a6a50a0e97f19b739aff4d67b5
Mark Vilrokx <mvilrokx@gmail.com>
2010-12-15 12:19:13 -0800

    Configured Sphinx for prod server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'vendor/plugins/thinking-sphinx/recipes/thinking_sphinx'
+
 # set :user, 'git'
 set :user, 'mark'
 set :domain, 'mark-server'
@@ -10,7 +12,8 @@ set :application, 'barrelrun'
 set :repository, "git@#{domain}:#{application}.git"
 # but this is what is in the book, just pick the one that works ...
 #set :repository, "#{user}@#{domain}:git/#{application}.git"
-set :deploy_to, "/home/#{user}/#{domain}"
+# set :deploy_to, "/home/#{user}/#{domain}"
+set :deploy_to, "/home/#{user}/#{domain}_production"
 
 # distribute your applications across servers (the instructions below put them
 # all on the same server, defined above as 'domain', adjust as necessary)
@@ -52,4 +55,18 @@ task :configure_database, :roles => :app do
 #     db_config = "#{deploy_to}/config/database.yml"
 #     run "cp #{db_config} #{release_path}/config/database.yml"
      run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+end
+
+task :before_update_code, :roles => [:app] do
+  thinking_sphinx.stop
+end
+
+task :after_update_code, :roles => [:app] do
+  symlink_sphinx_indexes
+  thinking_sphinx.configure
+  thinking_sphinx.start
+end
+
+task :symlink_sphinx_indexes, :roles => [:app] do
+  run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
 end
