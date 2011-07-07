d209ece02c5468cc2452871e5eaaa1aef01f877b
Mark Vilrokx <mvilrokx@gmail.com>
2011-07-07 14:09:40 -0700

    deploy hook fixe 4

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -5,8 +5,14 @@ run "ln -nfs #{shared_path}/config/sphinx #{release_path}/config/sphinx"
 run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config/sphinx.yml"
 
 # replace action_mailer host
+node[:engineyard][:environments][:instances] do |instance|
+  if instance[:framework_env]=="development"
+    host = instance[:public_hostname]
+    puts host
+  end
+end
+
 filepath = "#{release_path}/config/environments/development.rb"
 text = File.read(filepath)
-puts node[:engineyard]
-replace = text.gsub(/mark-server.dlinkddns.com:3000/, "#{node[:instance_role]}")
+replace = text.gsub(/mark-server.dlinkddns.com:3000/, host)
 File.open(filepath, "w") {|file| file.puts replace}
