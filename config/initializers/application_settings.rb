ac4f4787e44dcfcf4f00f70a13bbca3adb0ea106
Mark Vilrokx <mvilrokx@gmail.com>
2011-07-18 00:44:14 -0700

    changed youtube authorization keys to the barrelrun.com ones

--- config/initializers/application_settings.rb
+++ config/initializers/application_settings.rb
@@ -1,3 +1,8 @@
-YOUTUBE_CONFIG = {:consumer_key => "mark-server.dlinkddns.com", 
-                  :consumer_secret => "jv7hmvF1j73pNpfT6pYlVE7y", 
-                  :dev_key => 'AI39si6wyI6C04i613rvgRfxoxbm1yxn8AAKZi1cTWqXjSocqX30Yo5ratEjsPNpGM92Y5CaDNYPWkmHf0OVZ-ChUkHmVapNfg'}
+YOUTUBE_CONFIG = {:consumer_key => "www.barrelrun.com",
+                  :consumer_secret => "ewUXcSj3rH59UWo5jmzZ1K3l",
+                  :dev_key => "AI39si4ulcv2opfr9wACSx3ZTJKBPqxl17d5CJO3hvGww6FmVHCJw1nnMWkKpFsTC0U34IVCVFdfJrRmKUG5iKHyOPl61ej6fQ"}
+
+#YOUTUBE_CONFIG = {:consumer_key => "mark-server.dlinkddns.com",
+#                  :consumer_secret => "jv7hmvF1j73pNpfT6pYlVE7y",
+#                  :dev_key => 'AI39si6wyI6C04i613rvgRfxoxbm1yxn8AAKZi1cTWqXjSocqX30Yo5ratEjsPNpGM92Y5CaDNYPWkmHf0OVZ-ChUkHmVapNfg'}
+
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,8 @@
 #  provider :youtube, 'mark-server.dlinkddns.com', 'jv7hmvF1j73pNpfT6pYlVE7y'
 #end
 ActionController::Dispatcher.middleware.use OmniAuth::Builder do
-  provider :you_tube, 'mark-server.dlinkddns.com', 'jv7hmvF1j73pNpfT6pYlVE7y'
+#  provider :you_tube, 'mark-server.dlinkddns.com', 'jv7hmvF1j73pNpfT6pYlVE7y'
+  provider :you_tube, 'www.barrelrun.com', 'ewUXcSj3rH59UWo5jmzZ1K3l'
   provider :twitter, 'ZC7GVkVfCfIoPpgI6CrMA', '5cppBETFGH4JcvI8DBZ2dX1sTDwpf8frrzxjEtYUS0'
 end
 
