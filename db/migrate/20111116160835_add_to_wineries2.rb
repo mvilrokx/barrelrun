4c7806d1bfc24c318e23eb0b77af776545357d1b
Greg Sandford <gregory_sandford@msn.com>
2011-11-16 08:09:51 -0800

    x

---
+++ db/migrate/20111116160835_add_to_wineries2.rb
@@ -0,0 +1,12 @@
+class AddToWineries2 < ActiveRecord::Migration
+class RemoveFromWineries < ActiveRecord::Migration
+   def self.up
+    add_column :wineries, :descr, :text
+    add_column :wineries, :hours, :string
+  end
+
+  def self.down
+	remove_column :wineries, :descr
+	remove_column :wineries, :hours
+  end
+end
