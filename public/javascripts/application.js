de4c8c7852ababccc204ed755ee187f6546a0492
Greg Sandford <gregory_sandford@msn.com>
2011-11-22 14:07:57 -0800

    x

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -54,38 +54,6 @@ $(document).ready(function(){
    });
 });
 
-$(document).ready(function() {
-var adjustheight = 20;
-$("#adjust").toggle(function() {
-$('#mytext').css('height', 'auto').css('overflow', 'visible');
-$(this).text("less");
-}, function() {
-$('#mytext').css('height', adjustheight).css('overflow', 'hidden');
-$(this).text("more");
-});
-});
-
-$(function(){
-
-// The height of the content block when it's not expanded
-var adjustheight = 30;
-// The "more" link text
-var moreText = "+  More";
-// The "less" link text
-var lessText = "- Less";
-
-// Sets the .more-block div to the specified height and hides any content that overflows
-$(".more-less .more-block").css('height', adjustheight).css('overflow', 'hidden');
-
-// The section added to the bottom of the "more-less" div
-$(".more-less").append('[…] '); // Set the "More" text 
-$("a.adjust").text(moreText); 
-$(".adjust").toggle(function() { $(this).parents("div:first").find(".more-block").css('height', 'auto').css('overflow', 'visible'); // Hide the [...] when expanded 
-$(this).parents("div:first").find("p.continued").css('display', 'none'); $(this).text(lessText); }, function() { $(this).parents("div:first").find(".more-block").css('height', adjustheight).css('overflow', 'hidden'); 
-$(this).parents("div:first").find("p.continued").css('display', 'block'); $(this).text(moreText); }); });
-
-
-
 
 $(".jqueryui_date").live('focus', function() {
    $(this).datepicker({
