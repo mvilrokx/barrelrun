4afc5c9bee23696d26b55316ce7fa77e186094ef
Greg Sandford <gregory_sandford@msn.com>
2011-11-22 07:59:14 -0800

    x

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -65,6 +65,27 @@ $(this).text("more");
 });
 });
 
+$(function(){
+
+// The height of the content block when it's not expanded
+var adjustheight = 20;
+// The "more" link text
+var moreText = "+  More";
+// The "less" link text
+var lessText = "- Less";
+
+// Sets the .more-block div to the specified height and hides any content that overflows
+$(".more-less .more-block").css('height', adjustheight).css('overflow', 'hidden');
+
+// The section added to the bottom of the "more-less" div
+$(".more-less").append('[…] '); // Set the "More" text 
+$("a.adjust").text(moreText); 
+$(".adjust").toggle(function() { $(this).parents("div:first").find(".more-block").css('height', 'auto').css('overflow', 'visible'); // Hide the [...] when expanded 
+$(this).parents("div:first").find("p.continued").css('display', 'none'); $(this).text(lessText); }, function() { $(this).parents("div:first").find(".more-block").css('height', adjustheight).css('overflow', 'hidden'); 
+$(this).parents("div:first").find("p.continued").css('display', 'block'); $(this).text(moreText); }); });
+
+
+
 
 $(".jqueryui_date").live('focus', function() {
    $(this).datepicker({
