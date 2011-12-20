d475c043318d91ecc7158ce829b11c75a75ce9cc
Mark Vilrokx <mvilrokx@gmail.com>
2011-12-20 14:41:36 -0800

    removed new applicatin folder which was added by accident

--- new/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '8e23042b3d6c362aad14eb7796539235ce5d79caa1fb83bb1138141a54432242a710130ad88fa2a3d3eea927ecc5f488348c9e191612a009d05f745eeac504d1';
