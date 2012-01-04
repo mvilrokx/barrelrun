d2afd47928b7f63309bc631941abc061ac00e76d
Mark Vilrokx <mvilrokx@gmail.com>
2012-01-03 22:19:02 -0800

    changed deploy/before_migrate file for mail issue on EY

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,8 +18,8 @@ Barrelrun::Application.configure do
   config.action_mailer.raise_delivery_errors = true
 #  config.action_mailer.raise_delivery_errors = false
 
-#  config.action_mailer.delivery_method = :sendmail
-#  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
