1006cb7cf1497db94a02ba642f0de2b61618a5ea
Mark Vilrokx <mvilrokx@gmail.com>
2012-01-03 22:31:31 -0800

    re-enabled changing of development.rb file for action mailer host

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -4,22 +4,22 @@ require 'fileutils'
 run "ln -nfs #{shared_path}/config/sphinx #{release_path}/config/sphinx"
 run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config/sphinx.yml"
 
-#puts "replace action_mailer host on development environment"
-#host = nil
-#puts node[:environment][:framework_env]
-#if node[:environment][:framework_env]=="development"
-#  node[:engineyard][:environment][:instances].each do |instance|
-#    puts "========================="
-#    puts instance
-#    puts "========================="
-#    puts instance[:public_hostname]
-#    puts "========================="
-#    host = instance[:public_hostname]
-#  end
-#  filepath = "#{release_path}/config/environments/development.rb"
-#  text = File.read(filepath)
-#  replace = text.gsub(/localhost:3000/, host)
-#  File.open(filepath, "w") {|file| file.puts replace}
-#end
+puts "replace action_mailer host on development environment"
+host = nil
+puts node[:environment][:framework_env]
+if node[:environment][:framework_env]=="development"
+  node[:engineyard][:environment][:instances].each do |instance|
+    puts "========================="
+    puts instance
+    puts "========================="
+    puts instance[:public_hostname]
+    puts "========================="
+    host = instance[:public_hostname]
+  end
+  filepath = "#{release_path}/config/environments/development.rb"
+  text = File.read(filepath)
+  replace = text.gsub(/localhost:3000/, host)
+  File.open(filepath, "w") {|file| file.puts replace}
+end
 puts "Finished running deploy/before_migrate.rb"
 
