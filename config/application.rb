cd128b86463adc408b8c4bb64f184035329b18bb
Mark Vilrokx <mvilrokx@gmail.com>
2012-05-13 21:52:23 -0700

    trying heroku precompile again

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Barrelrun
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
     #
     config.assets.precompile += %w( polyfills.js )
 
